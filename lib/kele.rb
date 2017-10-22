b746f87423dca9edad0487e084d349a6fef5c570
Alex Erling <alex.erling@gmail.com>
2017-10-22 22:59:32 +0000

    Messaging completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -24,6 +24,28 @@ require './lib/roadmap.rb'
     @mentor_availability = JSON.parse(response.body)
   end
   
+  def get_messages(page_num=nil)
+    if page_num == nil
+      response = self.class.get(api_url("message_threads"), headers: { "authorization" => @auth_token })
+    else
+      response = self.class.get(api_url("message_threads?page=#{page_num}"), headers: { "authorization" => @auth_token })
+    end
+    @messages = JSON.parse(response.body)
+  end
+  
+  def create_message(user_id, recipient_id, subject, message)
+    response = self.class.post(api_url("messages"),
+      body: {
+        "user_id": user_id,
+        "recipient_id": recipient_id,
+        "subject": subject,
+        "stripped_text": message
+        },
+        headers: {"authorization" => @auth_token})
+    puts "The message has been sent!" if response.success?
+   end
+  
+  
    private
  
    def api_url(endpoint)
