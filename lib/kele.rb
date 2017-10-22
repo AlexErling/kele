f469cc577340bb640788a70ebd3f108c45252198
Alex Erling <alex.erling@gmail.com>
2017-10-22 21:57:31 +0000

    Retrieve users completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
  
  class Kele
    include HTTParty
@@ -6,11 +7,15 @@ require 'httparty'
  
    def initialize(email, password)
      response = self.class.post(api_url("sessions"), body: {"email": email, "password": password})
-     puts response.code
      raise "Invalid email or password" if response.code == 404
      @auth_token = response["auth_token"]
    end
- 
+   
+  def get_me
+    response = self.class.get(api_url('users/me'), headers: { "authorization" => @auth_token })
+    @user_data = JSON.parse(response.body)
+  end
+          
    private
  
    def api_url(endpoint)
